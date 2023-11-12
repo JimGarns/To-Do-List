54e691af00f5e9f2c60c6a31135a62f3c0a986d1
Jim Garner <hello@garner.dev>
2023-11-12 21:28:35 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('main-input')
+const button = document.getElementById('add-list')
+const buttonDelete = document.getElementById('delete')
+const listUl = document.getElementById('list-ul')
+
+button.addEventListener('click', () =>{
+
+
+    if(input.value.trim() !== '') {
+        listUl.innerHTML += `<li>${input.value} <button class = 'delete'>Delete</button></li>`
+    }
+
+    input.value = '';
+})
+
+listUl.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete')) {
+        // Remove the closest li parent of the clicked delete button
+        event.target.closest('li').remove();
+    }
+});
